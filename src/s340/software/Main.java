8ac282c30ce6fab56d98b40c81dca1fecdcb202a
Frank-Ochoa <Frank-Ochoa@users.noreply.github.com>
2018-09-12 20:33:12 -0700

    We good?

--- src/s340/software/Main.java
+++ src/s340/software/Main.java
@@ -23,22 +23,24 @@ public class Main
 
 		// create a program
 		ProgramBuilder b1 = new ProgramBuilder();
+		int sum = 140;
+		int i = 160;
 
 		b1.start(50);
 		b1.loadi(0);
-		b1.store(140);
+		b1.store(sum);
 		b1.tax();
 		b1.txa();
-		b1.store(160);
+		b1.store(i);
 		b1.subi(101);
 		b1.jpos(76);
 		b1.jzero(76);
-		b1.load(140);
-		b1.add(160);
-		b1.store(140);
+		b1.load(sum);
+		b1.add(i);
+		b1.store(sum);
 		b1.incx();
 		b1.jmp(56);
-		b1.load(140);
+		b1.load(sum);
 		b1.output();
 		b1.end();
 
@@ -71,6 +73,60 @@ public class Main
 		b2.jmp(202);
 		b2.end();
 
+		ProgramBuilder b3 = new ProgramBuilder();
+		int sum2 = 170;
+		int i2 = 180;
+
+		b3.start(300);
+		b3.loadi(0);
+		b3.store(sum2);
+		b3.tax();
+		b3.txa();
+		b3.store(i2);
+		b3.subi(101);
+		b3.jpos(328);
+		b3.jzero(328);
+		// 170 is the sum
+		b3.load(i2);
+		b3.mul(i2);
+		// Add the square of 160
+		b3.add(sum2);
+		b3.store(sum2);
+		b3.incx();
+		b3.jmp(306);
+		b3.load(sum2);
+		b3.output();
+		b3.end();
+
+		ProgramBuilder b4 = new ProgramBuilder();
+
+		int j2 = 550;
+		int k2 = 650;
+
+		b4.start(800);
+		b4.loadi(31);
+		b4.store(j2);
+		b4.subi(36);
+		b4.jpos(842);
+		b4.jzero(842);
+		b4.loadi(31);
+		b4.store(k2);
+		b4.tax();
+		b4.txa();
+		b4.store(k2);
+		b4.subi(36);
+		b4.jpos(836);
+		b4.jzero(836);
+		b4.load(j2);
+		b4.mul(k2);
+		b4.output();
+		b4.incx();
+		b4.jmp(816);
+		b4.load(j2);
+		b4.addi(1);
+		b4.jmp(802);
+		b4.end();
+
 		// Question 6
 
 	  /*int a = 121;
@@ -100,19 +156,17 @@ public class Main
 	  b1.jmp(30);
 	  b1.end();*/
 
-		ProgramBuilder b3 = new ProgramBuilder();
-		b3.start(800);
-		b3.loadi(10);
-		b3.output();
-		b3.end();
+
 
 		Program p1 = b1.build();
 		Program p2 = b2.build();
 		Program p3 = b3.build();
+		Program p4 = b4.build();
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
+		System.out.println(p4);
 		// schedule the program
-		os.schedule(List.of(p1, p2));
+		os.schedule(List.of(p1, p2, p3, p4));
 	}
 }
